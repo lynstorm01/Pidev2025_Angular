3ca2c213db74ff04e3fecf28de697a4042d5a37f
eounissi <emna.ounissi@esprit.tn>
2025-02-14 12:16:00 +0100

    feature stripe + create habitation devis

--- src/app/model/Devis.model.ts
+++ src/app/model/Devis.model.ts
@@ -1,9 +1,20 @@
+import { Paiement } from "./Paiement.model";
+import { TypeAssurance } from "./TypeAssurance";
+
 export interface Devis {
-    id?: number;
+    id: number;
     nomClient: string;
-    typeAssurance: string;
+    emailClient:string,
+    typeAssurance: TypeAssurance;
     dateDebut: string;
     dateFin: string;
     statut: string;
+    signe?: boolean; 
+    trancheAge:number;
+nationalite:string; 
+destination:string;
+montantTotal:number;
+    signature?: string; // Base64-encoded signature image 
+    paiements: Paiement[];
   }
   
