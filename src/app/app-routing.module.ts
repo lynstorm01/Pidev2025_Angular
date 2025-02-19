859b718db146cb63d6d0bf3ee54259cb7c6969f7
ala ouhichi <74491805+lynstorm01@users.noreply.github.com>
2025-02-19 01:40:31 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { HomeComponent } from './front/home/home.component';
+import { CreateSinComponent } from './front/Sinister/create-sin/create-sin.component';
+import { SinisterADComponent } from './admin/sinister-ad/sinister-ad.component';
 
 const routes: Routes = [
 
-
+  { path: 'create', component: CreateSinComponent },
   { path: 'admin', component: DashboardComponent, children: [
+    { path: 'sinister', component: SinisterADComponent}
     // Ajoute d'autres routes pour les autres composants CRUD ici
 ]},
 { path: 'home', component: HomeComponent, children: [
   // Ajoute d'autres routes pour les autres composants CRUD ici
 ]},
 
-{ path: '**', redirectTo: '/home' }, // Fallback route
-
 { path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: '**', redirectTo: '/home' }, // Fallback route
 ];
 
 @NgModule({
