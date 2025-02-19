81c951bd3730584bc571d5def346af8250ff355b
ala ouhichi <74491805+lynstorm01@users.noreply.github.com>
2025-02-19 01:40:21 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AdminComponent } from './admin/admin.component';
+
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { OffresComponent } from './admin/offres/offres.component';
 import { FooterComponent } from './front/footer/footer.component';
 import { HeaderComponent } from './front/header/header.component';
 import { HomeComponent } from './front/home/home.component';
@@ -23,18 +22,19 @@ import { RouterLink } from '@angular/router';
 import { MatSelectModule } from '@angular/material/select'; // ✅ Import MatSelectModule
 import { MatOptionModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
+import { SinisterADComponent } from './admin/sinister-ad/sinister-ad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    AdminComponent,
     DashboardComponent,
-    OffresComponent,
     HomeComponent,
-    HistorComponent
+    HistorComponent,
+    CreateSinComponent,
+    SinisterADComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +56,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     RouterLink,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
